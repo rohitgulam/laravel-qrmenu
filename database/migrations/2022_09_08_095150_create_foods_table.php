6ee79fad2122e909699fc85e1e559b11d8d0b287
rohitgulam <rohitsgulam@gmail.com>
2022-09-08 15:28:35 +0300

    created food migration

---
+++ database/migrations/2022_09_08_095150_create_foods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 5, 2 );
+            $table->string('description')->nullable();
+            $table->string('category')->default('Other');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+};
