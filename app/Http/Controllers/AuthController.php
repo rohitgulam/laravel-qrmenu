4ec538eca87aa8c0b2cea8b567b155ce7772668c
rohitgulam <rohitsgulam@gmail.com>
2022-09-11 08:43:19 +0300

    created auth(user) controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'username' => 'required|string|unique:users,username',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'username' => $fields['username'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('userAuthToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return[
+            'message' => 'Logged out'
+        ];
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string|',
+            // 'username' => 'required|string|unique:users,username',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Wrong Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('userAuthToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+    }
+}
